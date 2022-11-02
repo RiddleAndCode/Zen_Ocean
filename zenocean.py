1c02ab07cc6c14e5ebadd5c1569aa64a84b3a525
cybnon <stefan.weber93@googlemail.com>
2022-11-02 11:53:12 +0100

    commit Intermidiate state

--- zenocean.py
+++ zenocean.py
@@ -1,35 +1,41 @@
+from ocean_lib.models import data_nft_factory
+from ocean_lib.models.data_nft import DataNFT
+from ocean_lib.models.data_nft_factory import DataNFTFactoryContract
 from ocean_lib.ocean.ocean import Ocean
 from ocean_lib.example_config import ExampleConfig
 from ocean_lib.data_provider.data_service_provider import DataServiceProvider
-from ocean_utils.agreements.service_factory import ServiceDescriptor
+from ocean_lib.ocean.util import get_address_of_type
 from ocean_lib.web3_internal.wallet import Wallet
 from ocean_lib.models.datatoken import Datatoken
 from ocean_lib.web3_internal.currency import to_wei
 
 
+ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
+
+
 def get_config():
-    config = ExampleConfig.get_config('https://rinkeby.infura.io/v3/f0929b471ab04afc8d66a47bb52744f8')
+    config = ExampleConfig.get_config('https://eth-goerli.g.alchemy.com/v2/iNxmRj7JGw01fmAmgqh1rbAUyvhmhAmp')
     return config
 
 
-def run_scenario(data_hash, token_name="name", token_symbol="symbol"):
+def run_scenario(data_hash, data_nft_name: str, data_nft_symbol: str, dt_name: str, dt_symbol: str):
     try:
-        config = ExampleConfig.get_config("http://127.0.0.1:8545")
+        config = ExampleConfig.get_config('https://eth-goerli.g.alchemy.com/v2/iNxmRj7JGw01fmAmgqh1rbAUyvhmhAmp')
         ocean = Ocean(config)
         wallet = Wallet(ocean.web3, "b36504e44a35cff35a9fc80df9a9cee366f2058b73fe2a3fa0deab40347125f6",
-                        config.block_confirmations)
+                        ocean.config_dict.get('BLOCK_CONFIRMATIONS'), ocean.config_dict.get('TRANSACTION_TIMEOUT'))
         # ConfigProvider.set_config(config)
         # Web3Provider.init_web3(provider=get_web3_connection_provider(config.network_url))
         # ContractHandler.set_artifacts_path(config.artifacts_path)
 
         print(dir(wallet))
         print(wallet.address)
-        print(config.network_url)
-        print(config.provider_url)
-        print(config.network_url)
+        print(config.get('RPC_URL'))
+        print(config.get('PROVIDER_URL'))
 
-        data_token = Datatoken(token_name, token_symbol, from_wallet=wallet)
-        print(f'created new datatoken with address {data_token.address}')
+        data_nft = ocean.create_data_nft(data_nft_name, data_nft_symbol, wallet)
+        data_token = data_nft.create_datatoken(dt_name, dt_symbol, from_wallet=alice_wallet)
+
         token_address = data_token.address
         print(token_address)
 
@@ -46,7 +52,7 @@ def run_scenario(data_hash, token_name="name", token_symbol="symbol"):
         }
         metadata = {
             "main": {
-                "type": "dataset", "name": token_name, "author": "RIDDLE&CODE",
+                "type": "dataset", "name": dt_name, "author": "RIDDLE&CODE",
                 "license": "CC0: Public Domain", "dateCreated": date_created,
                 "files": [
                     {"index": 0, "contentType": "application/json", "url": get_config()["data_url"] + data_hash},
@@ -59,9 +65,8 @@ def run_scenario(data_hash, token_name="name", token_symbol="symbol"):
             }
         }
 
-        service_endpoint = DataServiceProvider.get_url(ocean.config)
-        download_service = ServiceDescriptor.access_service_descriptor(service_attributes, service_endpoint)
-        asset = ocean.assets.create(metadata, wallet, service_descriptors=[download_service],
+        service_endpoint = DataServiceProvider.get_url(ocean.config_dict)
+        asset = ocean.assets.create(metadata, wallet, provider_uri=[service_endpoint],
                                     data_token_address=token_address)
         assert token_address == asset.data_token_address
 
@@ -88,4 +93,7 @@ def run_scenario(data_hash, token_name="name", token_symbol="symbol"):
         print("Exception: " + str(e))
         return {"status": "NonValid", "data": str(e)}
 
-    # run_scenario( "0x376e05899a4ae00463a3a607c774069b7d6a647860dba723f39b735c91238ddf", token_name = "Drive&Stake-Token", token_symbol= "R3C-DS-T" )
+
+if __name__ == '__main__':
+    run_scenario( "0x376e05899a4ae00463a3a607c774069b7d6a647860dba723f39b735c91238ddf", data_nft_name="Drive&Stake-NFT", data_nft_symbol="R3C-DS-T-NFT", dt_name="Drive&Stake-Token", dt_symbol="R3C-DS-T" )
+
